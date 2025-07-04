42144ecd6832330b1ac953de0a5f8dd1ac629e61
Adithya306 <garukaadithya@gmail.com>
2025-07-04 12:56:15 +0530

    section 2 styles

---
+++ src/ui/AboutPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import About1 from './about1';
+import About2 from './about2';
+import About3 from './about3';
+
+const AboutPage = () => {
+  return (
+    <div>
+        <About1 />
+        <About2 />
+        <About3 />
+    </div>
+   
+  );
+};
+
+export default AboutPage;
