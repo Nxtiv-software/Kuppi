bb1c61a4efb0792ff49bc98ec60a68a940d03478
Adithya306 <garukaadithya@gmail.com>
2025-07-09 11:36:06 +0530

    student dashboard styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import Features from "./ui/Features"
 import HowItWorks from "./ui/HowItWorks"
 import Testimonials from "./ui/Testimonials"
 import Footer from "./ui/Footer"
+// import StudentOverview from "./features/student-dashboard/StudentOverview"
+import BrowseKuppis from "./features/students-dashboard/BrowseKuppi"
+import MySessions from "./features/students-dashboard/MySessions"
+import Overview from "./features/students-dashboard/Overview"
+import Dashboard from "./features/students-dashboard/StudentDasboard"
+import StudentDashboard from "./features/students-dashboard/StudentDasboard"
 
 
 function App() {
@@ -11,12 +17,14 @@ function App() {
 
   return (
     <>
-      <Header/>
+      {/* <Header/>
       <Hero/>
       <Features/>
       <HowItWorks/>
       <Testimonials/>
-      <Footer/>
+      <Footer/> */}
+
+      <StudentDashboard />
     </>
   )
 }
