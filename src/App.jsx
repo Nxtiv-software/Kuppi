395e834dc5c61c16c87e4c72972c77a772fc1900
Adithya306 <garukaadithya@gmail.com>
2025-07-21 14:56:42 +0530

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-// import Header from "./ui/Header"
-// import Hero from "./ui/Hero"
-// import Features from "./ui/Features"
-// import HowItWorks from "./ui/HowItWorks"
-// import Testimonials from "./ui/Testimonials"
-// import Footer from "./ui/Footer"
+import Header from "./ui/Header"
+import Hero from "./ui/Hero"
+import Features from "./ui/Features"
+import HowItWorks from "./ui/HowItWorks"
+import Testimonials from "./ui/Testimonials"
+import Footer from "./ui/Footer"
 // import StudentOverview from "./features/student-dashboard/StudentOverview"
 import StudentDashboard from "./features/students-dashboard/StudentDasboard"
 
@@ -12,12 +12,12 @@ function App() {
 
   return (
     <>
-      {/* <Header/>
+      <Header/>
       <Hero/>
       <Features/>
       <HowItWorks/>
       <Testimonials/>
-      <Footer/> */}
+      <Footer/>
 
       <StudentDashboard />
     </>
