8c48c729a89989134734988caf296b5d33f6cb77
Adithya306 <garukaadithya@gmail.com>
2025-08-15 09:32:02 +0530

    made some chnages in api.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+// AUTHENTICATION: Import Clerk components when deploying
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
 
@@ -11,11 +12,10 @@ import global_si from "./translations/si/global.json";
 
 import i18next from "i18next";
 import { I18nextProvider } from "react-i18next";
-import { AuthProvider } from "./context/AuthContext.jsx";
 
 i18next.init({
   interpolation: { escapeValue: false },
-  lng: "en",  // default language
+  lng: "en",
   resources: {
     en: {
       global: global_en,
@@ -24,10 +24,11 @@ i18next.init({
       global: global_si,
     }
   },
-  defaultNS: 'global',  // default namespace
-  ns: ['global'],       // namespaces used
+  defaultNS: 'global',
+  ns: ['global'],
 });
 
+// AUTHENTICATION: Uncomment when deploying with Clerk
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
@@ -38,11 +39,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <I18nextProvider i18n={i18next}>
-        {/* <AuthProvider> */}
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        {/* </AuthProvider> */}
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </I18nextProvider>
     </ClerkProvider>
   </React.StrictMode>
